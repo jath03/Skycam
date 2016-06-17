2f0e1e92ca330c7e46cad03938646ff2dfaa2c0a
jath03 <jackthughes@yahoo.com>
2016-06-16 19:26:52 -0700

    Making progress

--- skycam.py
+++ skycam.py
@@ -1,3 +1,9 @@
 from gpiozero import *
 endstop_front = Button(20)
 endstop_back = Button(16)
+drive = PWMOutputDevice(4, frequency=50)
+pan = PWMOutputDevice(2, frequency=50)
+tilt = PWMOutputDevice(3, frequency=50)
+class Skycam():
+    def forward(self, speed=50):
+        drive.value=1

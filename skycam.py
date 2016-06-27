7549c5ea744498fb9904b551f084c40400e14493
jath03 <jackthughes@yahoo.com>
2016-06-26 17:20:17 -0700

    blah

--- skycam.py
+++ skycam.py
@@ -1,9 +1,17 @@
 from gpiozero import *
 endstop_front = Button(20)
 endstop_back = Button(16)
-drive = PWMOutputDevice(4, frequency=50)
-pan = PWMOutputDevice(2, frequency=50)
-tilt = PWMOutputDevice(3, frequency=50)
+drive = PWMOutputDevice(2, frequency=50)
+pan = PWMOutputDevice(3, frequency=50)
+tilt = PWMOutputDevice(4, frequency=50)
 class Skycam():
-    def forward(self, speed=50):
-        drive.value=1
+    def forward(self, speed=1):
+        drive.value = .3
+    def backward(self, speed=1):
+        drive.value = .5
+    def drive(self):
+        pass
+    def PanTilt(self):
+        pass
+        
+        

88f67ac0e4a44d31bffba3d2b47656c65a6554e5
Jack <jackthughes@yahoo.com>
2016-06-26 17:17:16 -0700

    Lots of changes

--- skycam.py
+++ skycam.py
@@ -1,3 +1,5 @@
+import time
+import threading
 from gpiozero import *
 endstop_front = Button(20)
 endstop_back = Button(16)
@@ -17,7 +19,10 @@ class Skycam:
     def mainloop(self):
         Drive.control()
         
+        
 class Drive(Skycam):
+    def __init__(self):
+        th = threading.Thread(target=self.end())
     def forward(self, speed=1):
         drive.value = .3
     def backward(self, speed=1):
@@ -26,10 +31,23 @@ class Drive(Skycam):
         drive.off()
     def control(self):
         key = Skycam.keyInput()
-        print(key)
+        if key == 'Up':
+            self.forward()
+        elif key == 'Down':
+            self.backward()
+    def end(self):
+        if endstop_front or endstop_back == True:
+            self.stop()
             
 class PanTilt(Skycam):
     def center(self):
         tilt.value = .75
 #while True:
 #    Skycam.mainloop()
+while True:
+    print('-----')
+    print(endstop_front.is_pressed)
+    print(' ')
+    print(endstop_back.is_pressed)
+    print('-----')
+    time.sleep(1)

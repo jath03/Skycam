0534d4e356ad702292ea9f0ff61baf052426dadb
jath03 <jackthughes@yahoo.com>
2016-08-18 11:32:39 -0700

    changes

--- skycam.py
+++ skycam.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3 -i
-import os, tty, time, threading, gpiozero, sys, termios, curtsies, argparse, colors
+import os, tty, time, threading, gpiozero, sys, termios, curtsies, argparse, colors, picamera
 from server import MyServer
 from http.server import HTTPServer
 #The two endstops are defined as buttons
@@ -10,7 +10,8 @@ drive = gpiozero.PWMOutputDevice(2, frequency=300)
 pan = gpiozero.PWMOutputDevice(3, frequency=300)
 tilt = gpiozero.PWMOutputDevice(4, frequency=300)
 #The 4 RGB LEDs are wired together so can be defined as one
-leds = gpiozero.RGBLED(23, 25, 8)
+leds = gpiozero.RGBLED(24, 25, 8)
+leds.on()
 #Defining the LEDs colors
 red = 0, 1, 1
 green = 1, 0, 1
@@ -19,22 +20,37 @@ yellow = .1, .5, 1
 #adding the argument parser
 parser = argparse.ArgumentParser(description='Control the Skycam')
 parser.add_argument('-p', '--public', action='store_true', default=False, help='Makes the skycam stream availible over the internet')
+parser.add_argument('-s', '--secret', action='store_true', default=False, help='Makes the skycam more secretive')
 try:
     args = parser.parse_args()
 except:
-    pass
+    sys.exit(1)
 class Skycam:
     def __init__(self):
-        leds.blink(on_time=.5, off_time=.5, on_color=green, off_color=blue)
-        bg_server = threading.Thread(target = self.run)
-        stop_thread = threading.Thread(target=self.collision)
-        os.system('cd /home/jack/mjpg-streamer/mjpg-streamer-experimental && ./mjpg_streamer -i "./input_raspicam.so -fps 10" -o "./output_http.so -w .www -p 8081" &')
-        os.system('sudo -u jack motion')
-        stop_thread.start()
-        bg_server.start()
-        print('Initialized')
-        time.sleep(3)
-        leds.color = green
+        if args.secret != True:
+            leds.blink(on_time=.2, off_time=.2, on_color=green, off_color=blue)
+            server_thread = threading.Thread(target = self.run)
+            stop_thread = threading.Thread(target=self.collision)
+            cam_flip_thread = threading.Thread(target=self.flip)
+            os.system('cd /home/jack/mjpg-streamer/mjpg-streamer-experimental && ./mjpg_streamer -i "./input_raspicam.so -fps 10" -o "./output_http.so -w .www -p 8081" &')
+            os.system('sudo -u jack motion')
+            stop_thread.start()
+            server_thread.start()
+            cam_flip_thread.start()
+            print('Initialized')
+            time.sleep(1)
+            leds.color = green
+        else:
+            server_thread = threading.Thread(target=self.run)
+            stop_thread = threading.Thread(target=self.collision)
+            cam_flip_thread = threading.Thread(target=self.flip)
+            os.system('cd /home/jack/mjpg-streamer/mjpg-streamer-experimental && ./mjpg_streamer -i "./input_raspicam.so -fps 10" -o "./output_http.so -w .www -p 8081" &')
+            os.system('sudo -u jack motion')
+            stop_thread.start()
+            server_thread.start()
+            cam_flip_thread.start()
+            print('Initialized')
+            
     def center(self):
         pan.value = .15
         tilt.value = .7
@@ -74,27 +90,42 @@ class Skycam:
                     else:
                         self.off()
                 except gpiozero.exc.OutputDeviceBadValue:
-                    leds.value = red
-                    self.center()   
-                    time.sleep(.5)
-                    leds.value = green   
+                    if args.secret != True:
+                        leds.value = red
+                        self.center()   
+                        time.sleep(.5)
+                        leds.value = green   
+                    else:
+                        self.center()
     def collision(self):
         print('there was a collision')
         while True:
-            if endstop_front.value == True:
-                leds.color = yellow
-                print('you have hit something in the front')
-                drive.value = .3
-                time.sleep(.5)
-                drive.off()
-                leds.color = green
-            elif endstop_back.value == False:
-                leds.color = yellow
-                print('You have hit somthing in the back')
-                drive.value = .5
-                time.sleep(.5)
-                drive.off()
-                leds.color = green
+            if args.secret != True:
+                if endstop_front.value == True:
+                    leds.color = yellow
+                    print('you have hit something in the front')
+                    drive.value = .3
+                    time.sleep(.5)
+                    drive.off()
+                    leds.color = green
+                elif endstop_back.value == False:
+                    leds.color = yellow
+                    print('You have hit somthing in the back')
+                    drive.value = .5
+                    time.sleep(.5)
+                    drive.off()
+                    leds.color = green
+            else:
+                 if endstop_front.value == True:
+                    print('you have hit something in the front')
+                    drive.value = .3
+                    time.sleep(.5)
+                    drive.off()
+                 elif endstop_back.value == False:
+                    print('You have hit somthing in the back')
+                    drive.value = .5
+                    time.sleep(.5)
+                    drive.off()
     def run(self):
         print('starting server')
         if args.public == True:
@@ -107,8 +138,14 @@ class Skycam:
             server_address = ('192.168.1.200', 80)
             httpd = HTTPServer(server_address, MyServer.MyHandler)
             httpd.serve_forever()
-                
+    def flip(self):
+        while True:
+            if tilt.value == .35:
+                cam = picamera.PiCamera()
+                cam.vflip()                    
 def end():
-    os.system('killall motion')
-    sys.exit()
-
+    try:
+        os.system('killall motion')
+        sys.exit()
+    except:
+        sys.exit()

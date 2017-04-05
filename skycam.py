d80ee2f2133e516ad01bae19eab65ee590a68b85
jath03 <jackthughes@yahoo.com>
2017-04-05 11:20:51 -0700

    Initial commit

--- skycam.py
+++ skycam.py
@@ -2,3 +2,15 @@
 
 from communication import Communication
 from streamer import Streamer
+
+class Skycam(QObject):
+    def __init__(self, communication=None):
+        if not communication:
+            self.comm = Communication('0x50')
+        else:
+            self.comm = communication
+        self.streamer = Streamer()
+
+
+if __name__ == '__main__':
+    sk = Skycam()
--- streamer.py
+++ streamer.py
@@ -1,20 +1,15 @@
-# uncompyle6 version 2.9.10
-# Python bytecode 3.5 (3350)
-# Decompiled from: Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
-# [GCC 5.4.0 20160609]
-# Embedded file name: /home/jack/projects/local/testing/skycam/streamer.py
-# Compiled at: 2017-04-04 16:20:50
-# Size of source mod 2**32: 1160 bytes
 import sys
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QApplication
 from flask import Flask, render_template, Response
 import cv2
 import json
+
 fapp = Flask(__name__)
 
+
 class Streamer(QThread):
-    flipEvt = pyqtSignal()
+    flipEvt = pyqtSignal(object)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -29,15 +24,19 @@ class Streamer(QThread):
     def stream(self, cam):
         while True:
             s, img = cam.read()
-            cv2.imshow('Image', img)
+            if self.flipped:
+                (h, w) = img.shape[:2]
+                center = (w / 2, h / 2)
+                M = cv2.getRotationMatrix2D(center, 180, 1.0)
+                img = cv2.warpAffine(img, M, (w, h))
             s, jpg = cv2.imencode('.jpg', img)
-            cv2.imshow(jpg)
             frame = jpg.tobytes()
             yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n'
 
     def run(self):
         fapp.run(host='localhost', port=7777, debug=True)
 
+    @pyqtSlot(object)
     def flip(self, evt=None):
         print('filpping', evt)
         self.flipped = not self.flipped

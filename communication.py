a861d959b065a1000cc8239350b618099ddb0578
jath03 <jackthughes@yahoo.com>
2017-05-15 18:43:14 +0000

    Initial commit

--- communication.py
+++ communication.py
@@ -1,7 +1,23 @@
-
+import smbus
+import time
 
 class Communication(object):
 
     def __init__(self, master, address):
         self.address = address
-        print('done initiating communication')
+        self.master = master
+        self.bus = smbus.SMBus(1)
+    def write(msg):
+        for char in str(msg):
+            self.bus.write_byte(self.address, ord(char))
+        self.bus.write_byte(self.address, ord('\n'))
+    def read():
+        bytes_lst = []
+        while True:
+            time.sleep(.2)
+            b = self.bus.read_byte(self.address)
+            if b != ord('\n'):
+                bytes_lst.append(b)
+            else:
+                st = ''.join([chr(byte) for byte in bytes_lst])
+                return st
